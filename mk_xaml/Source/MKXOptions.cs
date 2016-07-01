310381d76b3a814ec47a8889f024dfcde6f4d652
surak8 <surak8@users.noreply.github.com>
2016-07-01 07:21:37 -0400

    oops, pushing option fix

--- mk_xaml/Source/MKXOptions.cs
+++ mk_xaml/Source/MKXOptions.cs
@@ -1,41 +1,64 @@
 using System.CodeDom.Compiler;
 using Microsoft.CSharp;
+using Microsoft.VisualBasic;
+using Microsoft.VisualC;
+using Microsoft.JScript;
 
 namespace NSMk_xaml {
-    public class MKXOptions : ICodeDomGenerationUtil {
-        #region ctor
-        public MKXOptions() : this("c#") { }
-        public MKXOptions(string whichType) {
-            switch (whichType.ToLower()) {
-                case "vb": provider = new Microsoft.VisualBasic.VBCodeProvider(); break;
-                case "cpp": case "c++": provider = new Microsoft.VisualC.CppCodeProvider10(); break;
-                case "js": provider = new Microsoft.JScript.JScriptCodeProvider(); break;
-                case "c#":
-                case "csharp":
-                default:
-                    provider = new CSharpCodeProvider();
-                    break;
-            }
-            useCompileUnit = false;
-        }
-        #endregion
+	public class MKXOptions : ICodeDomGenerationUtil {
 
-        #region properties
-        internal bool useCompileUnit { get; set; }
-        #endregion
+		enum LanguangeType {
+			VB,
+			CPP,
 
-        #region ICodeDomGenerationUtil implementation
-        public CodeGeneratorOptions options { get; private set; }
-        public CodeDomProvider provider { get; private set; }
+			JavaScript,
+			CSharp
+		}
 
-        public void createProvider() {
-            if (options == null) {
-                options = new CodeGeneratorOptions();
-                options.BlankLinesBetweenMembers = false;
-                options.ElseOnClosing = true;
-                options.IndentString = "\t";
-            }
-        }
-        #endregion ICodeDomGenerationUtil implementation
-    }
+		#region ctor
+		public MKXOptions() : this("c#") { }
+		public MKXOptions(string whichType) {
+			setLanguageByName(whichType);
+			useCompileUnit = false;
+		}
+		#endregion
+
+		internal void setLanguageByName(string aName) {
+			switch (aName.ToLower()) {
+				case "vb": provider = new VBCodeProvider(); break;
+				case "cpp": case "c++": provider = new CppCodeProvider10(); break;
+				case "js": provider = new JScriptCodeProvider(); break;
+				case "c#":
+				case "csharp":
+				default:
+					provider = new CSharpCodeProvider();
+					break;
+			}
+		}
+
+		#region properties
+		LanguangeType generationType { get; set; }
+		internal bool useCompileUnit { get; set; }
+		#endregion
+
+		#region ICodeDomGenerationUtil implementation
+		public CodeGeneratorOptions options { get; private set; }
+		public CodeDomProvider provider { get; private set; }
+
+		public void createProvider() {
+			if (options == null) {
+				options = new CodeGeneratorOptions();
+				options.BlankLinesBetweenMembers = false;
+				options.ElseOnClosing = true;
+				options.IndentString = "\t";
+			}
+			switch (generationType) {
+				case LanguangeType.VB: this.provider = new VBCodeProvider(); break;
+				case LanguangeType.CPP: this.provider = new CppCodeProvider10(); break;
+				case LanguangeType.JavaScript: this.provider = new JScriptCodeProvider(); break;
+				case LanguangeType.CSharp: this.provider = new CSharpCodeProvider(); break;
+			}
+		}
+		#endregion ICodeDomGenerationUtil implementation
+	}
 }
